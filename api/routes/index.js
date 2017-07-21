246a69886e3bb05088d76b24ed1bc0f9ad013562
Merilee Wheelock <eelirem@gmail.com>
2017-07-21 11:06:23 -0400

    back-end setup

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,5 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var config = require('../config/config');
+
+// var connection = mysql.createConnection({
+// 	host: config.host,
+// 	user: config.user,
+// 	password: config.password,
+// 	database: config.database
+// })
+
+// connection.connect();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
