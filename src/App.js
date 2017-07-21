0f38f0e82f917105a71d57fc631678e28d05962d
Merilee Wheelock <eelirem@gmail.com>
2017-07-21 10:43:19 -0400

    basic container outline

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import Navbar from './containers/Navbar';
+import Home from './containers/Home';
+import Survey from './containers/Survey';
+import Login from './containers/Login';
+import Register from './containers/Register';
+
 class App extends Component {
 	render() {
 		return (
 			<Router>
 				<div className="App">
+					<Navbar />
 					<div className="container">
-						<h1>traveList</h1>
+						<Route exact path="/" component={Home} />
+						<Route exact path="/survey" component={Survey} />
+						<Route exact path="/login" component={Login} />
+						<Route exact path="/register" component={Register} />
 					</div>
 				</div>
 			</Router>
---
+++ src/containers/Survey.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Survey extends Component{
+
+
+
+	render(){
+		return(
+			<div className="survey-box text-center col-sm-6 col-sm-offset-3">
+				<h1>Survey area</h1>
+			</div>
+		)
+	}
+}
+
+export default Survey;
