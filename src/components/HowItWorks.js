24785600853c984dff2a4b5b56ec9a93d5e39556
Marissa Monivis <mmonivis@gmail.com>
2017-08-02 15:33:31 -0400

    Updated about and How It Works

--- src/components/HowItWorks.js
+++ src/components/HowItWorks.js
@@ -7,19 +7,19 @@ class HowItWorks extends Component{
 			<div className="hiw-container-div text-center" id="howitworks">
 				<h1 className="text-center page-title">how it works</h1>
 				<div className="row how-boxes-row">
-					<div className="how-box col-sm-2 col-sm-offset-3">
+					<div className="how-box col-lg-2 col-lg-offset-3">
 						<div className="step-number"><h1>1</h1></div>
-						<div className="step-icon col-xs-6 col-xs-offset-3 col-md-6 col-md-offset-2 col-sm-12"><i class="fa fa-user-circle-o" aria-hidden="true"></i></div>
+						<div className="step-icon col-xs-6 col-xs-offset-3 col-md-"><i class="fa fa-user-circle-o" aria-hidden="true"></i></div>
 						<div className="steps">Create a user profile</div>
 					</div>
-					<div className="how-box col-sm-2">
+					<div className="how-box col-lg-2">
 						<div className="step-number"><h1>2</h1></div>
-						<div className="step-icon col-xs-6 col-xs-offset-3 col-md-6 col-md-offset-2 col-sm-12"><i class="fa fa-question-circle-o" aria-hidden="true"></i></div>
+						<div className="step-icon col-xs-6 col-xs-offset-3"><i class="fa fa-question-circle-o" aria-hidden="true"></i></div>
 						<div className="steps">Complete a short trip survey</div>
 					</div>
-					<div className="how-box col-sm-2">
+					<div className="how-box col-lg-2">
 						<div className="step-number"><h1>3</h1></div>
-						<div className="step-icon col-xs-6 col-xs-offset-3 col-md-6 col-md-offset-2 col-sm-12"><i class="fa fa-check-circle-o" aria-hidden="true"></i></div>
+						<div className="step-icon col-xs-6 col-xs-offset-3"><i class="fa fa-check-circle-o" aria-hidden="true"></i></div>
 						<div className="steps">Enjoy a stress-free trip!</div>
 					</div>
 				</div>
