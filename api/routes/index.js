00da4e785684cd826fbe2da44db355edb1ad561b
Merilee Wheelock <eelirem@gmail.com>
2017-08-02 11:28:01 -0400

    working on listview route

--- api/routes/index.js
+++ api/routes/index.js
@@ -14,100 +14,6 @@ var connection = mysql.createConnection({
 connection.connect();
 
 
-router.get('/profile', (req, res)=>{
-	profileQuery = `SELECT * FROM users
-	LEFT JOIN tripInfo ON users.id = tripInfo.uid`
-	connection.query(profileQuery, (error, results)=>{
-		if (error) throw error;
-		res.json(results);
-	})
-})
-
-router.post('/survey', (req, res)=>{
-	// console.log(req.body)
-	var tripType = req.body.tripType;
-    var tripSetting = req.body.tripSetting;
-    var destination = req.body.destination;
-    var tripDate = req.body.tripDate;
-    var children = req.body.children;
-    var token = req.body.token;
-    // console.log(token)
-
-    var tripSettingCheck = ""
-    if (tripSetting == 'beach'){
-    	tripSettingCheck += ' AND beach = 1'
-    }else if (tripSetting == 'winter'){
-    	tripSettingCheck += ' AND winter = 1'
-    }else if (tripSetting == 'camping'){
-    	tripSettingCheck += ' AND camping = 1'
-    }else if (tripSetting == 'formal'){
-    	tripSettingCheck += ' AND formal = 1'
-    }else if (tripSetting == 'international'){
-    	tripSettingCheck += ' AND international = 1'
-    }else if (tripSetting == 'business international'){
-    	tripSettingCheck += ' AND businessInternational = 1'
-    }else if (tripSetting == 'business casual'){
-    	tripSettingCheck += ' AND businessCasual = 1'
-    }else if (tripSetting == 'business formal'){
-    	tripSettingCheck += ' AND businessFormal = 1'
-    }
-
-    var childrenCheck = ""
-    if (children == 'children'){
-    	childrenCheck += `${tripSettingCheck} AND childJoining = 1`
-    }else if (children == 'babies'){
-    	childrenCheck += `${tripSettingCheck} AND babyJoining = 1`
-    }else if (children == 'none'){
-    	childrenCheck += `${tripSettingCheck} AND noChildren = 1`
-    }else if (children == 'childrenAndBabies'){
-    	childrenCheck += `${tripSettingCheck} AND childAndBaby = 1`
-    }
-
-    console.log(children)
-    console.log(childrenCheck)
-
-    const getUidQuery = `SELECT id from users WHERE token=?`
-	connection.query(getUidQuery, [token], (error,results)=>{
-		// console.log(token)
-		if(error) throw error;
-		if(results.length == 0 ){
-			res.json({msg:"badToken"})
-		}else{
-			// console.log(results[0].id)
-			const addToTripsQuery = `INSERT INTO tripInfo (uid, tripType, tripSetting, destination, tripDate, children)
-				VALUES (?,?,?,?,?,?)`
-			connection.query(addToTripsQuery, [results[0].id, tripType, tripSetting, destination, tripDate, children], (error2,results2)=>{
-				// const tripInfoId = results2.insertId
-				// connection.query()
-				if (error2) throw error;
-				res.json({
-					tripInfoId: results2.insertId
-				})
-				console.log(results2)
-			})
-		}
-	})
-})
-
-router.post('/listview', (req,res)=>{
-	console.log(req.body)
-
-
-
-
-	// surveyId = req.body.surveyId
-	// token = req.body.token
-	// console.log(surveyId)
-	// console.log(token)
-	// const createListQuery = `SELECT * from packList WHERE 1 ${childrenCheck}`
-	// console.log(createListQuery)
-	// connection.query(createListQuery, (error3,results3)=>{
-	// 	// console.log(results3)
-	// 	if (error3) throw error3;
-	// 	res.json(results3);
-	// })
-})	
-
 router.post('/register', (req, res)=>{
 	var email = req.body.email;
 	var password = bcrypt.hashSync(req.body.password);
@@ -182,6 +88,107 @@ router.post('/login', (req, res)=>{
 		}
 	})
 })
+
+router.get('/profile', (req, res)=>{
+	profileQuery = `SELECT * FROM users
+	LEFT JOIN tripInfo ON users.id = tripInfo.uid`
+	connection.query(profileQuery, (error, results)=>{
+		if (error) throw error;
+		res.json(results);
+	})
+})
+
+router.post('/survey', (req, res)=>{
+	// console.log(req.body)
+	var tripType = req.body.tripType;
+    var tripSetting = req.body.tripSetting;
+    var destination = req.body.destination;
+    var tripDate = req.body.tripDate;
+    var children = req.body.children;
+    var token = req.body.token;
+    // console.log(token)
+
+    var tripSettingCheck = ""
+    if (tripSetting == 'beach'){
+    	tripSettingCheck += ' AND beach = 1'
+    }else if (tripSetting == 'winter'){
+    	tripSettingCheck += ' AND winter = 1'
+    }else if (tripSetting == 'camping'){
+    	tripSettingCheck += ' AND camping = 1'
+    }else if (tripSetting == 'formal'){
+    	tripSettingCheck += ' AND formal = 1'
+    }else if (tripSetting == 'international'){
+    	tripSettingCheck += ' AND international = 1'
+    }else if (tripSetting == 'business international'){
+    	tripSettingCheck += ' AND businessInternational = 1'
+    }else if (tripSetting == 'business casual'){
+    	tripSettingCheck += ' AND businessCasual = 1'
+    }else if (tripSetting == 'business formal'){
+    	tripSettingCheck += ' AND businessFormal = 1'
+    }
+
+    var childrenCheck = ""
+    if (children == 'children'){
+    	childrenCheck += `${tripSettingCheck} AND childJoining = 1`
+    }else if (children == 'babies'){
+    	childrenCheck += `${tripSettingCheck} AND babyJoining = 1`
+    }else if (children == 'none'){
+    	childrenCheck += `${tripSettingCheck} AND noChildren = 1`
+    }else if (children == 'childrenAndBabies'){
+    	childrenCheck += `${tripSettingCheck} AND childAndBaby = 1`
+    }
+
+    console.log(children)
+    console.log(childrenCheck)
+
+    const getUidQuery = `SELECT id from users WHERE token=?`
+	connection.query(getUidQuery, [token], (error,results)=>{
+		// console.log(token)
+		if(error) throw error;
+		if(results.length == 0 ){
+			res.json({msg:"badToken"})
+		}else{
+			// console.log(results[0].id)
+			const addToTripsQuery = `INSERT INTO tripInfo (uid, tripType, tripSetting, destination, tripDate, children)
+				VALUES (?,?,?,?,?,?)`
+			connection.query(addToTripsQuery, [results[0].id, tripType, tripSetting, destination, tripDate, children], (error2,results2)=>{
+				// const tripInfoId = results2.insertId
+				// const listResultsQuery = `SELECT id FROM tripInfo
+				// 	INNER JOIN packList ON `
+				// connection.query()
+				if (error2) throw error;
+				res.json({
+					tripInfoId: results2.insertId
+				})
+				console.log(results2)
+			})
+		}
+	})
+})
+
+router.post('/listview', (req,res)=>{
+	surveyId = req.body.surveyId;
+	token = req.body.token;
+	const currentTripQuery = `SELECT * FROM tripInfo WHERE id = ?`
+	connection.query(currentTripQuery, [surveyId], (error,results)=>{
+		if (error) throw error;
+		res.json(results)
+	})
+
+
+	// surveyId = req.body.surveyId
+	// token = req.body.token
+	// console.log(surveyId)
+	// console.log(token)
+	// const createListQuery = `SELECT * from packList WHERE 1 ${childrenCheck}`
+	// console.log(createListQuery)
+	// connection.query(createListQuery, (error3,results3)=>{
+	// 	// console.log(results3)
+	// 	if (error3) throw error3;
+	// 	res.json(results3);
+	// })
+})	
+
 
 
 
